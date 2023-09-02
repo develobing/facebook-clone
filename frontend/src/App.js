137161a47cc09355b7e725f08027ce37bf13a461
Robin <philoin88@gmail.com>
2023-09-03 00:46:36 +0900

    Add Protected Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
 import Login from './pages/login';
 import Profile from './pages/profile';
-import Home from './pages/home';
+import LoggedInRoutes from './routes/LoggedInRoutes';
+import NotLoggedInRoutes from './routes/NotLoggedInRoutes';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
 
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/routes/LoggedInRoutes.js
@@ -0,0 +1,7 @@
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function LoggedInRoutes() {
+  const { user } = useSelector((state) => ({ ...state }));
+  return user ? <Outlet /> : <Navigate to="/login" />;
+}
