d7286eb933ed0e153572a923aca6cacc87a3822f
Robin <philoin88@gmail.com>
2023-09-07 22:41:29 +0900

    Add Post components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useReducer, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import CreatePostPopup from './components/createPostPopup';
@@ -10,9 +11,53 @@ import Reset from './pages/reset';
 import LoggedInRoutes from './routes/LoggedInRoutes';
 import NotLoggedInRoutes from './routes/NotLoggedInRoutes';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'POSTS_REQUEST':
+      return { ...state, loading: true, error: '' };
+
+    case 'POSTS_SUCCESS':
+      return { ...state, loading: false, posts: action.payload, error: '' };
+
+    case 'POSTS_ERROR':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+}
+
 function App() {
   const { user } = useSelector((state) => ({ ...state }));
   const [visible, setVisible] = useState(false);
+  const [{ loading, error, posts }, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: '',
+    posts: [],
+  });
+
+  const getAllPosts = async () => {
+    try {
+      dispatch({ type: 'POSTS_REQUEST' });
+
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/getAllPosts`,
+
+        {
+          headers: { Authorization: `Bearer ${user.token}` },
+        }
+      );
+
+      dispatch({ type: 'POSTS_SUCCESS', payload: data });
+    } catch (error) {
+      console.log('getAllPosts() - error', error);
+      dispatch({ type: 'POSTS_ERROR', payload: error.response.data.message });
+    }
+  };
+
+  useEffect(() => {
+    if (user) getAllPosts();
+  }, [user]);
 
   return (
     <div className="App">
@@ -22,7 +67,10 @@ function App() {
 
       <Routes>
         <Route element={<LoggedInRoutes />}>
-          <Route path="/" element={<Home setVisible={setVisible} />} />
+          <Route
+            path="/"
+            element={<Home posts={posts} setVisible={setVisible} />}
+          />
           <Route path="/profile" element={<Profile />} />
           <Route path="/activate/:token" element={<Activate />} />
         </Route>
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -5,9 +5,10 @@ import LeftHome from '../../components/home/left';
 import RightHome from '../../components/home/right';
 import SendVerification from '../../components/home/sendVerification';
 import Stories from '../../components/home/stories';
+import Post from '../../components/post';
 import './style.css';
 
-export default function Home({ setVisible }) {
+export default function Home({ posts, setVisible }) {
   const { user } = useSelector((user) => ({ ...user }));
 
   return (
@@ -20,6 +21,11 @@ export default function Home({ setVisible }) {
         <Stories />
         {!user.verified && <SendVerification user={user} />}
         <CreatePost user={user} setVisible={setVisible} />
+
+        <div className="posts"></div>
+        {posts.map((post) => (
+          <Post key={post._id} post={post} user={user} />
+        ))}
       </div>
 
       <RightHome user={user} />
